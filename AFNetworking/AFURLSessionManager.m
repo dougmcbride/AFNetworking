1d6e197fe0e81e71f761cdf06e80e9e4079d06c6
Doug McBride <doug.mcbride@disney.com>
2021-11-12 11:38:32 -0800

    Remove swizzling code to see if this fixes our video issue

--- AFNetworking/AFURLSessionManager.m
+++ AFNetworking/AFURLSessionManager.m
@@ -379,6 +379,8 @@ static NSString * const AFNSURLSessionTaskDidSuspendNotification = @"com.alamofi
             7) If the current class implementation of `resume` is not equal to the super class implementation of `resume` AND the current implementation of `resume` is not equal to the original implementation of `af_resume`, THEN swizzle the methods
             8) Set the current class to the super class, and repeat steps 3-8
          */
+
+      /* Don't actually swizzle, see if this fixes a video issue we're having.
         NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
         NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
 #pragma GCC diagnostic push
@@ -401,6 +403,7 @@ static NSString * const AFNSURLSessionTaskDidSuspendNotification = @"com.alamofi
         
         [localDataTask cancel];
         [session finishTasksAndInvalidate];
+        */
     }
 }
 
